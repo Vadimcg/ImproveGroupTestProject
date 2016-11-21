e445150dac8103e462fce4ae883eeb93acd2a6b9
vadimcg <vadim-derevich@yandex.ru>
2016-11-21 10:00:10 +0300

    Finished  activity of registration

--- app/src/main/java/com/group/improve/improvegrouptestproject/RegistrationActivity.java
+++ app/src/main/java/com/group/improve/improvegrouptestproject/RegistrationActivity.java
@@ -1,7 +1,6 @@
 package com.group.improve.improvegrouptestproject;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegistrationActivity extends Activity {
@@ -9,6 +8,6 @@ public class RegistrationActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_registration);
     }
 }
--- app/src/main/java/com/group/improve/improvegrouptestproject/SignInActivity.java
+++ app/src/main/java/com/group/improve/improvegrouptestproject/SignInActivity.java
@@ -10,7 +10,7 @@ public class SignInActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
     }
 }
 
